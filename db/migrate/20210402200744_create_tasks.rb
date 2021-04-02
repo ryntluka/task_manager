f427b10a276e78149037f78172058ec205001017
Lukas Rynt <ryntluka@fit.cvut.cz>
2021-04-02 22:09:15 +0200

    [ADD] Generate models for Task, Tag and Project

---
+++ db/migrate/20210402200744_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.text :description
+      t.boolean :id_done
+      t.string :attachment
+
+      t.timestamps
+    end
+  end
+end
