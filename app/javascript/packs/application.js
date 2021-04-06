0387d0e2193dcd99a2e72973b388637308947e20
Lukas Rynt <ryntluka@fit.cvut.cz>
2021-04-06 22:42:31 +0200

    [EDIT] structure for javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,21 +1,12 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require bootstrap-sprockets
-//= require turbolinks
-//= require_tree .
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
 
-import 'bootstrap'
+require("jquery");
+import "bootstrap";
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 import 'bootstrap-icons/font/bootstrap-icons.css'
-import "../stylesheets/application"
