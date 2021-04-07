eadb167e4d1afc27475b7761f8e637d1a4ed9bb0
Lukas Rynt <ryntluka@fit.cvut.cz>
2021-04-08 01:55:08 +0200

    [ADD] some user acceptance test data

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :task do
     user
     title { Faker::Book.title }
+    description { Faker::Lorem.paragraphs(number: 10).join("\n") }
     is_done { Faker::Boolean.boolean }
   end
 end
