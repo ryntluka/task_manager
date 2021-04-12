dcaa20f5056ae8648819943a84d5f9f2c549e362
lukasrynt <lukasrynt@gmail.com>
2021-04-12 12:53:32 +0200

    [RENAME] tag to task in test

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Tag, type: :model do
+RSpec.describe Task, type: :model do
   subject { build(:task) }
   describe 'validations' do
     it { should validate_presence_of(:title) }
